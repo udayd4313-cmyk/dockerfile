d899475c5ee02dbeaf02767d9eb5ca3b6997c979
uday <udayd4313@email.com>
2026-03-25 23:14:04 +0530

    fixed port for railway

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Uday! Docker + Render working 🚀"
+    return "Hello Uday! Docker + Railway working 🚀"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
