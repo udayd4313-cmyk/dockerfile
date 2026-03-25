31e09d4582c34fdaa3ad442c1020234646bb4c9d
uday <udayd4313@email.com>
2026-03-25 15:54:16 +0530

    updated for render deployment

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
 FROM python:3.9
+
 WORKDIR /app
-COPY app.py .
+
+COPY . .
+
+RUN pip install -r requirements.txt
+
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello Uday! Docker project running 🚀")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello Uday! Docker + Render working 🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
