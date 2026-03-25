6f93b563e23fdf5beda16320e4f61da77c4eadc4
uday <udayd4313@email.com>
2026-03-25 23:20:48 +0530

    fixed railway port issue

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Uday! Docker + Railway working 🚀"
+    return "Hello Uday! Railway working 🚀"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))  # IMPORTANT LINE
     app.run(host="0.0.0.0", port=port)
